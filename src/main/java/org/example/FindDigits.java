f89c377340a827c354d78c69652a7b11f7e9c4ea
Bryan Gould <bcgould04@users.noreply.github.com>
2024-02-26 13:29:42 -0600

    more solutions

--- src/main/java/org/example/FindDigits.java
+++ src/main/java/org/example/FindDigits.java
@@ -6,26 +6,16 @@ import java.util.List;
 public class FindDigits {
 
     public static int findDigits(int n) {
+
        int divisors = 0;
-
-        /*
-        String[] digits = String.valueOf(n).split("");
-        for (String digit : digits) {
-            int d = Integer.parseInt(digit);
-            if (d != 0 && n % d == 0) {
-                divisors++;
-            }
-        }*/
-
-        int originalN = n;
-        while (n > 0) {
-            int digit = n % 10;
-            if (digit != 0 && originalN % digit == 0) {
-                divisors++;
-            }
+       int originalN = n;
+       while (n > 0) {
+           int digit = n % 10;
+           if (digit != 0 && originalN % digit == 0) {
+               divisors++;
+           }
             n /= 10;
-        }
-        return divisors;
+       }
+       return divisors;
     }
-
 }
